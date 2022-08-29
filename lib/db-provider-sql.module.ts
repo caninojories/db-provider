97cb7a88bc3c23f8e8b5ba5a0d7ca44a29fe8896
caninojories <caninojories@hotmail.com>
2022-08-30 01:17:54 +0800

    Add name for multiple database

--- lib/db-provider-sql.module.ts
+++ lib/db-provider-sql.module.ts
@@ -19,17 +19,20 @@ type DbProviderSqlAsyncModuleOptions = {
   useFactory: (
     ...args: any[]
   ) => Promise<DbProviderSqlOptions> | DbProviderSqlOptions;
+  name?: string;
 } & Pick<ModuleMetadata, 'imports'> &
   Pick<FactoryProvider, 'inject'>;
 
 @Module({})
 export class DbProviderSqlModule {
   static async registerAsync({
+    name = 'default',
     useFactory,
     inject,
   }: DbProviderSqlAsyncModuleOptions): Promise<DynamicModule> {
     return TypeOrmModule.forRootAsync({
       useFactory,
+      name,
       dataSourceFactory: async (options: DataSourceOptions | undefined) => {
         const dataSource = await new DataSource(
           options as DataSourceOptions,
